c5dccaec487e576f7ea5c5e42cdb17b82a12d1c1
Lakshya2857 <lakshyasangwan3111@gmail.com>
2026-07-10 23:24:43 +0530

    currency_convertor

---
+++ 06currencyconvertor/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-3xl bg-orange-500'>Chai With React_Vai</h1>
+
+    </>
+  )
+}
+
+export default App
