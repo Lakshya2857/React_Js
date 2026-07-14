839753247df62fd6c2835c6cb08aaf8320b20656
Lakshya2857 <lakshyasangwan3111@gmail.com>
2026-07-14 18:33:56 +0530

    Routes_and_Routers

---
+++ 07reactRouter/src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="py-16">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img
+                            src="https://imgs.search.brave.com/7iSDvLjujal5LmWCNcw7C2nRdCvhayonid-YH6-euH4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTIw/OTAzODA4Ni9waG90/by9sdW5jaC10aW1l/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz1nbUJleDFsZFk0/dzdVRGwyek93Tmt1/Sklwb1FZNEo2dHBv/cEpBT1ZMMk9JPQ"
+                            alt="image"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                            React development is carried out by passionate developers
+                        </h2>
+                        <p className="mt-6 text-gray-600">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                        </p>
+                        <p className="mt-4 text-gray-600">
+                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
